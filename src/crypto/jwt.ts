071e865f727da914893d06261c3206d6d1715403
Wim Onderbeke <wim.onderbeke@zoho.eu>
2022-08-19 11:01:58 +0200

    Add token signing, verifying and decoding

--- src/crypto/jwt.ts
+++ src/crypto/jwt.ts
@@ -1,4 +1,3 @@
-
 /*
 JSON Web Tokens consist of three parts xxxxx.yyyyy.zzzzz:
 * Header: Type of token and signature algorithm
@@ -23,21 +22,19 @@ import crypto from "crypto";
 import fs from "fs";
 import path from "path";
 
-const pubKeyPath = path.resolve('../../jwt.key.pub');
-const pubKeyFile = fs.readFileSync(pubKeyPath, 'utf8');
-console.log(pubKeyFile);
+const pubKeyPath = path.resolve("../../jwt.key.pub");
+const pubKeyFile = fs.readFileSync(pubKeyPath, "utf8");
 const pubKey = crypto.createPublicKey(pubKeyFile);
-const privateKeyPath = path.resolve('../../jwt.key');
-const privateKeyFile = fs.readFileSync(privateKeyPath, 'utf8');
-console.log(privateKeyFile);
+const privateKeyPath = path.resolve("../../jwt.key");
+const privateKeyFile = fs.readFileSync(privateKeyPath, "utf8");
 const privateKey = crypto.createPrivateKey(privateKeyFile);
 
 export type PublicKey = crypto.KeyObject;
 export type PrivateKey = crypto.KeyObject;
 
 export interface Keypair {
-    privateKey: PrivateKey;
-    publicKey: PublicKey;
+  privateKey: PrivateKey;
+  publicKey: PublicKey;
 }
 
 export interface SignOptions {
@@ -58,26 +55,39 @@ function createSignature(data: string, key: PrivateKey) {
   return signer.sign(key, "base64url");
 }
 
-export async function sign({ data, privateKey, options = {} }: SignInput) {
+export async function createJWT({ data, privateKey, options = {} }: SignInput) {
   const expiresIn = Date.now() + (options.expiresIn || 15 * 60000);
 
-  const encodedHeader = Buffer.from(JSON.stringify({ alg: "RS256", typ: "JWT" })).toString("base64url");
-  const encodedData = Buffer.from(JSON.stringify({ ...data, exp: expiresIn })).toString("base64url");
+  const encodedHeader = Buffer.from(JSON.stringify({ alg: "RS256", typ: "JWT" })).toString(
+    "base64url",
+  );
+  const encodedData = Buffer.from(JSON.stringify({ ...data, exp: expiresIn })).toString(
+    "base64url",
+  );
   const signature = createSignature(`${encodedHeader}.${encodedData}`, privateKey);
 
   return `${encodedHeader}.${encodedData}.${signature}`;
 }
 
-//export async function decode(token: JWT, publicKey: PublicKey) {
-    //[encodedHeader, encodedData, signature] = token.split(".");
+export async function decode(token: JWT, publicKey: PublicKey) {
+  let [encodedHeader, encodedData, signature] = token.split(".");
 
-    //return crypto.verify("SHA256", Buffer.from(`${encodedHeader}.${encodedData}`), publicKey, signature);
-//}
+  return Buffer.from(encodedData, "base64url").toString("utf8");
+}
 
-//export async function verify(token: JWT, publicKey: PublicKey) {
-    //[encodedHeader, encodedData, signature] = token.split(".");
+export async function verify(token: JWT, publicKey: PublicKey) {
+  let [encodedHeader, encodedData, signature] = token.split(".");
 
-    //return crypto.verify("SHA256", Buffer.from(`${encodedHeader}.${encodedData}`), publicKey, signature);
-//}
-
-console.log(await sign({ data: { sub: "1234567890", name: "John Doe", admin: true }, privateKey, options: { expiresIn: 15 * 60000 } }));
+  return crypto
+    .createVerify("RSA-SHA256")
+    .update(`${encodedHeader}.${encodedData}`)
+    .verify(publicKey, signature, "base64url");
+}
+let token = await createJWT({
+  data: { sub: "1234567890", name: "John Doe", admin: true },
+  privateKey,
+  options: { expiresIn: 15 * 60000 },
+});
+console.log(token);
+console.log(await decode(token, pubKey));
+console.log(await verify(token, pubKey));
